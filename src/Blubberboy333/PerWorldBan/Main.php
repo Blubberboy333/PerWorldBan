fe1145434690295a8484e4dab6935613e7c2d3d7
Blubberboy333 <thecowmooman@gmail.com>
2016-04-18 10:26:18 -0400

    Update Main.php

--- src/Blubberboy333/PerWorldBan/Main.php
+++ src/Blubberboy333/PerWorldBan/Main.php
@@ -1,1 +1,21 @@
+<?php
 
+namespace Blubberboy333\PerWorldBan;
+
+use pocketmine\level\Level;
+use pocketmine\level\Position;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\utils\Config;
+use pocketmine\utils\TextFormat;
+use pocketmine\Player;
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\event\entity\EntityLevelChangeEvent;
+
+class Main extends PluginBase implements Listener{
+    public function onEnable(){
+        $this->saveDefaultConfig();
+        $this->getLogger()->info(TextFormat::GREEN."Done!");
+    }
+}
